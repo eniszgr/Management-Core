bcacb465dc3689abea3636c38c42a85906d986ef
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-07-05 18:51:10 +0300

    adding new sector page

---
+++ Management-Core/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using Management_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Management_Core.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var result = c.Sectors.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult newSector()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult newSector(Sector s)
+        {
+
+            c.Sectors.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
