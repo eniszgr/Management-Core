49d83b0f9c3f00d26f53467802a87d7c08f21f17
Enis ÖZGÜR <zgr.enis@gmail.com>
2024-07-05 19:29:43 +0300

    sectors and employees pages

---
+++ Management-Core/Controllers/TheEmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Management_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Management_Core.Controllers
+{
+    public class TheEmployeeController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var results = c.Employees.Include(x=>x.Sectors).ToList();
+            return View(results);
+        }
+    }
+}
